57efc1a33418d9341389a1d2e7d9fa01e4951c08
VICTORIA MARQUES SOBRINHO <M.SOBRINHO@csi.aneas.msft>
2019-09-06 21:22:37 -0300

    adm

---
+++ administrador.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="css/grid.css">
+   <link rel="stylesheet" href="css/style.css">
+	<title>Administrador</title>
+</head>
+<body>
+		<main>
+			<section id="form_adm">
+				<div class="logo">
+					<img src="img/logo.jpg" alt="JF Arquitetura">
+				</div>
+					<div class="container">
+					<h2 class="subtitulo">Seja bem-vindo!</h2>				
+						<form action="">
+							<div class="form-card ">
+								<label>E-mail</label>
+								<input type="email">
+
+								<label>Senha</label>
+								<input type="password">
+								
+								<a href="">Esqueceu a senha?</a>
+								
+								<button type="submit" class="btn-adm">Entrar</button>
+							</div>
+						</form>
+					</div>
+				</section>
+		</main>
+</body>
+</html>
